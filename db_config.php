798295f7979cdc95afdd96259108bff5c76ee242
jaypathak14 <>
2020-04-07 01:48:36 +0530

    php
    php

---
+++ db_config.php
@@ -0,0 +1,11 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname="sole";
+// Create connection
+$conn = new mysqli($servername, $username, $password,$dbname);
+
+//$mysqli = new mysqli(localhost, root1, root , sole);
+?>
